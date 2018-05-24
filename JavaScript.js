2c549320ea56e1107dd23ffa95840e0ecc76f105
kristina kirby <kristinakirby@mac.com>
2018-05-24 14:15:37 -0600

    initial commit

---
+++ JavaScript.js
@@ -0,0 +1,35 @@
+// Write a script function that returns a passed string with letters in alphabetical order.
+// Example string : 'webmaster'
+// Expected Output : 'abeemrstw'
+
+// function alphabet_order(str)
+//   {
+// return str.split('').sort().join('');
+//   }
+// console.log(alphabet_order("webmaster"));
+
+//---------------
+// Write a script that accepts a string as a parameter and find the longest word within the string.
+// Example string : 'Web Development Tutorial'
+// Expected Output : 'Development'
+// function length_order(str)
+//   {
+//  return str.split('').sort().join('');
+//    }
+//  console.log(length_order("webmaster"));
+//---------------
+
+// Write a script that checks whether a passed string is palindrome or not. 
+// A palindrome is word, phrase, or sequence that reads the same backward as forward, 
+// e.g., madam or nurses run. Make sure to take care of spaces and capital letters.
+
+
+var a = new Array()
+a.sort();
+a.sort(function(b,a)  {
+    return b-a;
+    });
+a.sort(funtion(b,a) {return b-a}); 
+console.log a
+
+
---
+++ ruby.rb
@@ -0,0 +1,45 @@
+require 'pry'
+# if in vim: ":wq"
+# if stuck in loop: "!exit"
+# if you want to move to next pry: "exit"
+
+
+
+# puts 'The quick brown fox'
+# str = gets.chomp
+# puts str.downcase()
+## -----------------------------
+# Write a script function that returns a passed string with letters in alphabetical order.
+# Example string : 'webmaster'
+# Expected Output : 'abeemrstw'
+####done in java
+## -------------------------------
+# Write a script that accepts a string as a parameter and find the longest word within the string.
+# Example string : 'Web Development Tutorial'
+# Expected Output : 'Development'
+
+# def cntstr
+# puts "Give me a sentence, and I'll put how many chareters "
+# ##local variable
+# userinfo = gets.strip
+# ##determine if i am using a string or array method
+# userinfo 
+
+def fizzbuzz
+    binding.pry 
+    arr = []
+    (1...100).each do |x|
+    if x  %
+    binding.pry
+     arr << "Fizzbuzz"  
+    elsif
+     arr << "Fizz" 
+    else arr << x
+    binding.pry
+    end
+end
+puts arr
+end
+fizzbuzz
+
+
